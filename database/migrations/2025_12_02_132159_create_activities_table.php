3707b001cc6177184f06b32efd65a17726ad1e5d
Naylin Acosta <atenasplasencia@gmail.com>
2025-12-02 08:44:03 -0500

    commit

---
+++ database/migrations/2025_12_02_132159_create_activities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->date('due_date');
+            $table->enum('status', ['pendiente', 'en_progreso', 'entregada', 'calificada']);
+            $table->foreignId('course_id')->constrained('courses');
+            $table->foreignId('assigned_to_user_id')->constrained('users')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
